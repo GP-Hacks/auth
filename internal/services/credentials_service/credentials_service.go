8887801e872f1c6fed61b14243411d4b5676db30
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-05-01 21:04:26 +0300

    added interfaces for repositories

---
+++ internal/services/credentials_service/credentials_service.go
@@ -0,0 +1,25 @@
+package credentials_service
+
+import (
+	"context"
+
+	"github.com/GP-Hacks/auth/internal/models"
+)
+
+type (
+	ICredentialsRepository interface {
+		GetById(ctx context.Context, id int64) (*models.Credentials, error)
+		GetCountByEmail(ctx context.Context, email string) (int64, error)
+		Create(ctx context.Context, m *models.Credentials) (int64, error)
+		GetByEmail(ctx context.Context, email string) (*models.Credentials, error)
+	}
+
+	ITokensRepository interface {
+		GetById(ctx context.Context, id int64) (*models.Token, error)
+		Create(ctx context.Context, m *models.Token) (int64, error)
+		Update(ctx context.Context, m *models.Token) error
+		Delete(ctx context.Context, id int64) error
+		GetCountById(ctx context.Context, id int64) (int64, error)
+		GetByJTI(ctx context.Context, jti string) (*models.Token, error)
+	}
+)
