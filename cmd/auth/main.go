b26d6fbc0a18e5e00fd6d27db38fe2977a978468
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-05-09 20:55:23 +0300

    added main file

---
+++ cmd/auth/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net"
+
+	"github.com/GP-Hacks/auth/internal/config"
+	"github.com/GP-Hacks/auth/internal/service_provider"
+	"github.com/GP-Hacks/auth/internal/utils/logger"
+	proto "github.com/GP-Hacks/proto/pkg/api/auth"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	config.LoadConfig("./config")
+	logger.SetupLogger()
+	serviceProvider := service_provider.NewServiceProvider()
+
+	log.Info().Msg("Init app")
+
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	reflection.Register(grpcServer)
+
+	proto.RegisterAuthServiceServer(grpcServer, serviceProvider.AuthController())
+
+	list, err := net.Listen("tcp", ":"+config.Cfg.Grpc.Port)
+	if err != nil {
+		log.Fatal().Msg("Failed start listen port")
+	}
+
+	err = grpcServer.Serve(list)
+	if err != nil {
+		log.Fatal().Msg("Failed serve grpc")
+	}
+}
