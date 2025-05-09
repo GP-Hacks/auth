93b670c60bce65b61f23c49e524c6f6963b61f84
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-05-09 16:54:05 +0300

    upd domain errors

--- internal/services/errors.go
+++ internal/services/errors.go
@@ -3,7 +3,9 @@ package services
 import "errors"
 
 var (
-	NotFound       = errors.New("Not found")
-	InternalServer = errors.New("Internal server")
-	AlreadyExists  = errors.New("Already exists")
+	NotFound           = errors.New("Not found")
+	InternalServer     = errors.New("Internal server")
+	AlreadyExists      = errors.New("Already exists")
+	InvalidCredentials = errors.New("Invalid credentials")
+	InvalidToken       = errors.New("Invalid token")
 )
