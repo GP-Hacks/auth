7997420c8279cac45dedb92b26b45ec06ecb42e5
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-06-05 14:57:08 +0300

    upd email repo

--- internal/repositories/email_tokens_repository/get.go
+++ internal/repositories/email_tokens_repository/get.go
@@ -2,27 +2,26 @@ package email_tokens_repository
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
-	"github.com/GP-Hacks/auth/internal/services"
+	"github.com/GP-Hacks/auth/internal/utils/errs"
 	"github.com/go-redis/redis/v8"
-	"github.com/rs/zerolog/log"
 )
 
 func (r *EmailTokensRepository) GetUserID(ctx context.Context, token string) (int64, error) {
 	val, err := r.client.Get(ctx, token).Result()
 	if err != nil {
 		if err == redis.Nil {
-			return 0, services.NotFound
+			return 0, errs.NotFoundError
 		}
-		log.Error().Msg(err.Error())
-		return 0, services.InternalServer
+
+		return 0, errors.Join(errs.SomeError, err)
 	}
 	var userID int64
 	_, err = fmt.Sscan(val, &userID)
 	if err != nil {
-		log.Error().Msg(err.Error())
-		return 0, services.InternalServer
+		return 0, errors.Join(errs.SomeError, err)
 	}
 
 	return userID, nil
--- internal/repositories/email_tokens_repository/save.go
+++ internal/repositories/email_tokens_repository/save.go
@@ -2,19 +2,18 @@ package email_tokens_repository
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/GP-Hacks/auth/internal/config"
-	"github.com/GP-Hacks/auth/internal/services"
-	"github.com/rs/zerolog/log"
+	"github.com/GP-Hacks/auth/internal/utils/errs"
 )
 
 func (r *EmailTokensRepository) Save(ctx context.Context, token string, userID int64) error {
 	val := fmt.Sprint(userID)
 	err := r.client.Set(ctx, token, val, config.Cfg.Redis.TokensTTL).Err()
 	if err != nil {
-		log.Error().Msg(err.Error())
-		return services.InternalServer
+		return errors.Join(errs.SomeError, err)
 	}
 	return nil
 }
