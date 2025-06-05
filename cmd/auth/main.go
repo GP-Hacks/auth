f0ab9d05daab91280e89ed2bc8c3e8fb9260527f
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-06-05 14:57:46 +0300

    upd main go

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -4,12 +4,12 @@ import (
 	"net"
 
 	"github.com/GP-Hacks/auth/internal/config"
+	"github.com/GP-Hacks/auth/internal/controllers/grpc/auth_controller"
 	"github.com/GP-Hacks/auth/internal/service_provider"
 	"github.com/GP-Hacks/auth/internal/utils/logger"
 	proto "github.com/GP-Hacks/proto/pkg/api/auth"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -20,7 +20,7 @@ func main() {
 
 	log.Info().Msg("Init app")
 
-	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(auth_controller.UnaryDomainErrorInterceptor()))
 	reflection.Register(grpcServer)
 
 	proto.RegisterAuthServiceServer(grpcServer, serviceProvider.AuthController())
